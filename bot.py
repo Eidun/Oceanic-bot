6266e0d1d3e6b442785c7d25ab98937567bbfa7b
Eidun <migferf@gmail.com>
2018-12-03 20:29:17 +0100

    V1: Initial version

---
+++ bot.py
@@ -0,0 +1,46 @@
+import asyncio
+import sys
+import traceback
+import datetime
+import discord
+from discord.ext import commands
+import os
+
+
+# Key
+token = os.environ['token']
+
+print(token)
+
+description = '''Intercambia mensajes malvados'''
+
+modules = {'forwarding'}
+
+bot = commands.Bot(command_prefix='$', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('Oceanic-Bot starting...')
+    print(bot.user.name)
+    print(bot.user.id)
+    await bot.change_presence(game=discord.Game(name='con información'))
+
+    print('Loading cogs...')
+    if __name__ == '__main__':
+        modules_loaded = 0
+        for module in modules:
+            try:
+                bot.load_extension('cogs.' + module)
+                print('\t' + module)
+                modules_loaded += 1
+            except Exception as e:
+                traceback.print_exc()
+                print(f'Error loading the extension {module}', file=sys.stderr)
+        print(str(modules_loaded) + '/' + str(modules.__len__()) + ' modules loaded')
+        print('Systems 100%')
+    print('------')
+
+
+# Test bot
+bot.run(token)
---
+++ cogs/forwarding.py
@@ -0,0 +1,108 @@
+import discord
+from discord.ext import commands
+
+
+class Anuncios:
+    channels = {}
+
+    default_channel = '410366860354256897'
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.load_channels()
+
+    def load_channels(self):
+        for channel in self.bot.get_all_channels():
+            self.channels[channel.id] = channel
+        print(self.channels)
+
+    @commands.command(pass_context=True, hidden=True)
+    async def load(self, ctx):
+        self.load_channels()
+
+    @commands.command(pass_context=True, hidden=True)
+    async def set_default(self, ctx, channel_id):
+        self.default_channel = channel_id
+        print(self.default_channel)
+
+    @commands.command(pass_context=True)
+    async def anuncio(self, ctx, channel=None):
+        """Comienza el anuncio. La configuración después"""
+        # Get the author
+        user = ctx.message.author
+        # Obtener canal destinatario
+        if (channel is None):
+            channel = self.default_channel
+        try:
+            send_channel = self.channels[channel]
+        except:
+            await self.bot.send_message(user, 'No se ha podido encontrar el canal destinatario')
+            return
+        # Mensaje a enviar
+        mensaje_final = await self.construir(user)
+
+        # Envío del mensaje
+        try:
+            await self.bot.send_message(send_channel, mensaje_final)
+        except:
+            await self.bot.send_message(user, 'No se ha podido enviar el comunicado al canal seleccionado')
+
+    @commands.command(pass_context=True)
+    async def anuncio_privado(self, ctx, member):
+        """Comienza el anuncio privado. La configuración después"""
+        # Get the author
+        user = ctx.message.author
+        # Obtener destinatario
+        send_member = list(filter(lambda x: x.name == member, list(self.bot.get_all_members())))
+        if send_member.__len__() == 0:
+            await self.bot.send_message(user, 'No se ha encontrado al destinatario del comunicado')
+            return
+        send_member = send_member[0]
+        # Mensaje a enviar
+        mensaje_final = await self.construir(user)
+        # Envío del mensaje
+        try:
+            await self.bot.send_message(send_member, mensaje_final)
+        except:
+            await self.bot.send_message(user, 'No se ha podido enviar el comunicado al canal seleccionado')
+
+    async def construir(self, user):
+        # Solo desde los mensajes privados
+        def private_channel(message):
+            return message.channel.type == discord.ChannelType.private
+
+        # Mensaje a anunciar
+        msg: discord.Message = await self.bot.send_message(user, 'Escriba el anuncio que desea comunicar:')
+        anuncio: discord.Message = await self.bot.wait_for_message(timeout=50, author=user, check=private_channel)
+        if anuncio is None:
+            return
+        # Firma del anunciante
+        msg: discord.Message = await self.bot.send_message(user, 'Recibido. ¿Desea añadir una firma?')
+        await self.bot.add_reaction(msg, u"\u2705")
+        await self.bot.add_reaction(msg, u"\u274E")
+
+        # Solo uno de los dos emojis que se esperan
+        def allowed_reaction(reaction, user):
+            return (reaction.emoji == u"\u2705") or (reaction.emoji == u"\u274E")
+
+        # Emoji respuesta
+        response = await self.bot.wait_for_reaction(timeout=30, user=user, message=msg, check=allowed_reaction)
+
+        # Añadir firma
+        firma = 'Anónimo'
+        if (response is None or response.reaction.emoji == u"\u274E"):
+            await self.bot.send_message(user, 'Muy bien, será anónimo. En unos instantes se hará el comunicado.')
+        else:
+            await self.bot.send_message(user, 'Perfecto. ¿Qué nombre debería figurar?')
+            firma: discord.Message = await self.bot.wait_for_message(timeout=50, author=user, check=private_channel)
+            if firma is not None:
+                firma = firma.content
+            await self.bot.send_message(user, f'Muy bien, {firma}. En unos instantes se hará el comunicado.')
+
+        # Construir mensaje respuesta
+        mensaje_final = f'```ini\n\n[{firma}]\n{anuncio.content}\n```'
+        return mensaje_final
+
+
+def setup(bot):
+    bot.add_cog(Anuncios(bot))
